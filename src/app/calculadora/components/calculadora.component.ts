00bea06f86a7cede974c49f26dd5967fc410fe12
anderson.antunes <andersonlantunes@gmail.com>
2021-05-31 20:59:27 -0300

    Created calculadora service

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CalculadoraService } from '../services';
+
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
-  styleUrls: ['./calculadora.component.css']
+  styleUrls: ['./calculadora.component.css'],
 })
 export class CalculadoraComponent implements OnInit {
+  constructor(private calculadoraService: CalculadoraService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
